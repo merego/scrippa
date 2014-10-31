bb5e63b733d97c85602f610362baad3f223604e2
Paolo Mereghetti <paolo.mereghetti@gmail.com>
2014-10-31 15:59:49 +0100

    Figs

--- R/CG-auto-analysis/MakeFiguresPLOS.R
+++ R/CG-auto-analysis/MakeFiguresPLOS.R
@@ -22,8 +22,8 @@ n_of_runs <- length(files)
 # Load Reference distributions
 
 # SET SYSTEM PARAMs
-IBI <- FALSE
-MC <- TRUE
+IBI <- TRUE
+MC <- FALSE
 Alpha<-TRUE 
 
 MCIBI<-FALSE
@@ -410,6 +410,20 @@ if (MC) {
     Nstart <- Nstart + Nparams
   }
 }
+
+
+# 7. Boxplot of RMS(Loss)
+# Use here ggplot2
+filename <- paste("FigForPaper/RMSLossFunction.eps")
+postscript(filename, height=5, width=10)
+par(mar=c(4,4,1,1),mgp=c(5,2,0),oma=c(1.0,1.0,1.0,1.0),new=FALSE)
+DMT <- as.data.frame(diff(mt))
+boxplot(DMT)
+#axis(side = 1, tick = TRUE, labels=FALSE)
+#axis(side = 2, font=2, cex.axis=1.5, cex.lab=2.2, labels=TRUE)
+dev.off()
+
+
 # AcceptedIterations<-vector()
 # # Load distributions and loss functions
 # for (run in 1:n_of_runs) {
