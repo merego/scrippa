130c8329d1cd573e98b9fa7051072900b23fa473
Paolo Mereghetti <paolo.mereghetti@gmail.com>
2014-11-02 20:59:05 +0100

    Plos

--- R/CG-auto-analysis/RMSLossBoxPlots.R
+++ R/CG-auto-analysis/RMSLossBoxPlots.R
@@ -8,13 +8,17 @@ DMTMC <- DMT
 rm(DMT)
 
 DMTIBI[,6]<-"IBI"
-DMTMC[,6]<-"MC"
+DMTMC[,6]<-"MCSA-IBI"
 DMT<-rbind(DMTMC,DMTIBI)
 colnames(DMT)[6]<-"Method"
 DMTMelt<-melt(DMT)
 
-ggplot(DMTMelt,aes(x=Method,y=value,fill=variable)) + geom_boxplot()
-ggplot(DMTMelt,aes(x=variable,y=value,fill=Method)) + geom_boxplot() +
+#ggplot(DMTMelt,aes(x=Method,y=value,fill=variable)) + geom_boxplot()
+cbbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+# To use for fills, add
+
+BoxPlot <- ggplot(DMTMelt,aes(x=variable,y=value,fill=Method)) + geom_boxplot() +
+  scale_fill_manual(values=cbbPalette) +
 theme(panel.background = element_rect(fill = 'white'), 
       axis.ticks.x = element_line(size=1.2, colour="black"),
       axis.title.x = element_blank(),
@@ -24,4 +28,7 @@ theme(panel.background = element_rect(fill = 'white'),
       axis.text.y  = element_text(angle=0, vjust=0.5, size=15, face="bold", colour="black"),        
       plot.title = element_text(lineheight=3, face="bold", color="black", size=30),
       legend.title  = element_blank(),
-      legend.text = element_text(lineheight=3, face="bold", color="black", size=15))
+      legend.text = element_text(lineheight=3, face="bold", color="black", size=15),
+      legend.position = c(0.2,0.9))
+print(BoxPlot)
+ggsave(BoxPlot,file="RMSLossBoxPlot.pdf")
