a6992cd74f893891762e2a7f1e58d9792cbb09e2
paolo <paolo.mereghetti@gmail.com>
2016-02-05 10:33:14 +0100

    lpgcustom

--- R/CustomFunctions.R
+++ R/CustomFunctions.R
@@ -95,3 +95,22 @@ grid.arrange.aligned <- function(plots) {
 
   do.call("grid.arrange", c(grobs, ncol = 1))
 }
+
+localMaxima <- function(x) {
+  # Use -Inf instead if x is numeric (non-integer)
+  y <- diff(c(-.Machine$integer.max, x)) > 0L
+  rle(y)$lengths
+  y <- cumsum(rle(y)$lengths)
+  y <- y[seq.int(1L, length(y), 2L)]
+  if (x[[1]] == x[[2]]) {
+    y <- y[-1]
+  }
+  y
+}
+
+
+# append to list
+lappend <- function (lst, ...){
+  lst <- c(lst, list(...))
+  return(lst)
+}
